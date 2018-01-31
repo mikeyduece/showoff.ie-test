c3dee8cf65f46a9fce7af3ed8071c4df8bb59572
Mike Heft <mikeheft@gmail.com>
2018-01-31 16:29:40 -0700

    Work on login and logout with styling jumbotron for each

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,12 +4,14 @@ class SessionsController < ApplicationController
   end
 
   def create
+    require 'pry'; binding.pry
     user = User.from_omniauth(request.env['omniauth.auth'])
     session[:user_id] = user.id
     redirect_to user_path(user)
   end
 
   def destroy
+    require 'pry'; binding.pry
     session.clear
     redirect_to root_path
   end
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   get 'auth/:provider/callback', to: 'sessions#create'
   get 'auth/failure',            to: redirect('/')
-  get 'signout',                 to: 'sessions#destroy', as: 'signout'
+  delete 'signout',                 to: 'sessions#destroy', as: 'signout'
 
   resources :users, only: [:show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
