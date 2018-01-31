0b04becce65ca018692a371f1d1a44e48e360890
Mike Heft <mikeheft@gmail.com>
2018-01-31 14:40:57 -0700

    Add users and controller with method to find with omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,3 +1,17 @@
 class SessionsController < ApplicationController
 
+  def new
+  end
+
+  def create
+    user = User.from_omniauth(request.env['omniauth.auth'])
+    session[:user_id] = user.id
+    redirect_to user_path(user)
+  end
+
+  def destroy
+    session.clear
+    redirect_to root_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ApplicationRecord
+
+  def self.from_omniauth(auth)
+    user = User.where(uid: auth['data']['uid']).first
+    if user.nil?
+      user = User.create(uid: auth['data']['id'],
+                         username: auth['data']['username'],
+                         profile_pic: auth['data']['profile_picture'],
+                         bio: auth['data']['bio'])
+    end
+    user
+  end
 end
