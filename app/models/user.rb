47de3c78ae292184beb5f2242864c4226dc4a3ab
Mike Heft <mikeheft@gmail.com>
2018-02-02 11:00:59 -0700

    Refactor test to show that user can login with instagram

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,14 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     user = User.where(uid: auth['uid']).first
-    if user && user.token != auth["credentials"]["token"]
-      user.token = auth["credentials"]["token"]
-      user.save
+    if user && user.token != auth['credentials']['token']
+      user = User.update(uid: auth["uid"],
+                         full_name: auth["info"]["name"],
+                         profile_pic: auth["info"]["image"],
+                         nickname: auth['info']['nickname'],
+                         bio: auth['info']['bio'],
+                         token: auth["credentials"]["token"])
+
     elsif user.nil?
       user = User.create(uid: auth["uid"],
                          full_name: auth["info"]["name"],
@@ -13,6 +18,8 @@ class User < ApplicationRecord
                          bio: auth['info']['bio'],
                          token: auth["credentials"]["token"])
     end
+
     user
   end
+
 end
--- spec/features/user_can_view_basic_profile_information_spec.rb
+++ spec/features/user_can_view_basic_profile_information_spec.rb
@@ -1,18 +1,19 @@
 require 'rails_helper'
 
 feature 'As a User I can' do
-  scenario 'view basic profile information' do
+  scenario 'login with instagram' do
     VCR.use_cassette('login') do
       stub_omniauth
 
-      visit '/'
-      click_on('Login with Instagram', match: :first)
+      visit root_path
+      click_link('Login with Instagram')
+
       user = User.last
 
       expect(page).to have_content(user.nickname)
       expect(page).to have_content(user.full_name)
       expect(page).to have_content(user.bio)
-      expect(page).to have_content(user.profile_picture)
+      expect(page.find('.rounded-circle')['src']).to have_content(user.profile_pic)
     end
   end
 end
