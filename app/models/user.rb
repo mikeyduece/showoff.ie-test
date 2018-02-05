0aa96f35e58f54704d7ca180315f526abb23bb9f
Mike Heft <mikeheft@gmail.com>
2018-02-05 11:04:48 -0700

    Add guards to check for current data coming in from omniauth hash

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,14 +3,17 @@ class User < ApplicationRecord
 
   def self.from_omniauth(auth)
     user = User.where(uid: auth['uid']).first
-    if user && user.token != auth['credentials']['token']
+    if user && user.token != auth['credentials']['token'] ||
+        user.profile_pic != auth['info']['image'] ||
+        user.bio != auth['info']['bio'] ||
+        user.nickname != auth['info']['nickname'] ||
+        user.full_name != auth['info']['name']
       user = User.update(uid: auth["uid"],
                          full_name: auth["info"]["name"],
                          profile_pic: auth["info"]["image"],
                          nickname: auth['info']['nickname'],
                          bio: auth['info']['bio'],
-                         token: auth["credentials"]["token"])
-
+                         token: auth["credentials"]["token"]).first
     elsif user.nil?
       user = User.create(uid: auth["uid"],
                          full_name: auth["info"]["name"],
@@ -19,7 +22,6 @@ class User < ApplicationRecord
                          bio: auth['info']['bio'],
                          token: auth["credentials"]["token"])
     end
-
     user
   end
 
@@ -32,5 +34,6 @@ class User < ApplicationRecord
     def biography
       self.bio = self.bio.split
     end
+
 
 end
