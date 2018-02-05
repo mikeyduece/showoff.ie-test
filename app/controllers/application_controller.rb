5485438d1f4c615ff2a09ea2b0a681e573ee3e44
Mike Heft <mikeheft@gmail.com>
2018-02-05 12:09:11 -0700

    Add guard to only allow logged in user to see their own info not other users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,11 @@ class ApplicationController < ActionController::Base
   before_action :current_user
 
   helper_method :require_user,
-                :current_user,
-                :current_user_bio
+    :current_user,
+    :current_user_bio
 
   def require_user
-    unless current_user
+    unless current_user || (current_user.present? && current_user.id == params[:id])
       flash[:notice] = "You Must Log in or Register to View this Page"
       redirect_to root_path
     end
@@ -21,5 +21,4 @@ class ApplicationController < ActionController::Base
   def current_user_bio
     JSON.parse(current_user.bio) if current_user
   end
-
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,11 @@
 class UsersController < ApplicationController
+  before_action :require_user
 
   def index
 
   end
 
   def show
-
+    render file: '/public/404' unless current_user.id == params[:id].to_i
   end
 end
